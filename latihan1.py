d0f4223e5ea2416e0605aebb71037692702ee452
hugunsalau <91172333+hugunsalau@users.noreply.github.com>
2021-09-29 10:09:02 +0700

    upload latihan 1

---
+++ latihan1.py
@@ -0,0 +1,26 @@
+from math import sqrt
+formula = input('Sisi mana yang ingin anda hitung = ')
+
+if formula =='c':
+    sisiA = int(input('Masukkan panjang sisi a = '))
+    sisiB = int(input('Masukkan panjang sisi b = '))
+    
+    sisiC = sqrt(sisiA*sisiA + sisiB*sisiB)
+    print('Panjang sisi c adalah', sisiC)
+
+elif formula =='a':
+    sisiC = int(input('Masukkan panjang sisi c = '))
+    sisiB = int(input('Masukkan panjang sisi b = '))
+    
+    sisiA = sqrt(sisiC*sisiC - sisiB*sisiB)
+    print('Panjang sisi a adalah', sisiA)
+    
+elif formula =='b':
+    sisiC = int(input('Masukkan panjang sisi c = '))
+    sisiA = int(input('Masukkan panjang sisi a = '))
+    
+    sisiB = sqrt(sisiC*sisiC - sisiA*sisiA)
+    print('Panjang sisi b adalah', sisiB)
+    
+else:
+    print('Input Salah')
